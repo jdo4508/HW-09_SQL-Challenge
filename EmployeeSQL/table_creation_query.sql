153551640dfad3d8105a00d8bbef5ffbc1f6f2f9
Jennifer Borgogno <60944812+jdo4508@users.noreply.github.com>
2020-05-02 13:32:09 -0400

    updated table creation query

--- EmployeeSQL/table_creation_query.sql
+++ EmployeeSQL/table_creation_query.sql
@@ -6,37 +6,37 @@ DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS titles;
 
 CREATE TABLE departments 
-   (dept_no VARCHAR(30) ,
-  dept_name VARCHAR(30));
+   (dept_no VARCHAR(30) NOT NULL,
+  dept_name VARCHAR(30) NOT NULL);
 
 CREATE TABLE dept_emp 
-	 (emp_no INT,
-	dept_no VARCHAR(30),
-	from_date DATE,
-	to_date DATE);
+	 (emp_no INT NOT NULL,
+	dept_no VARCHAR(30) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL);
 	
 CREATE TABLE dept_manager
-	(dept_no VARCHAR(30),
-	emp_no INT,
-	from_date DATE,
-	to_date DATE);
+	(dept_no VARCHAR(30) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL;
 
 CREATE TABLE employees
-	 (emp_no INT,
-	birth_date DATE,
-	first_name VARCHAR(30),
-	last_name VARCHAR(30),
-	gender VARCHAR,
-	hire_date DATE);
+	 (emp_no INT NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT NULL);
 	
 CREATE TABLE salaries
-	 (emp_no INT,
-	salary INT,
-	from_date DATE,
-	to_date DATE);
+	 (emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL);
 	
 CREATE TABLE titles
-	(emp_no INT,
-	title VARCHAR(30),
-	from_date DATE,
-	to_date DATE);
+	(emp_no INT NOT NULL,
+	title VARCHAR(30) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL);
